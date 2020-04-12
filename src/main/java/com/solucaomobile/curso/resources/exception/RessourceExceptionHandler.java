af9affbdd25c358f2d77e85bb68ce2bbf45d3217
valderez <valderezpe@gmail.com>
2020-04-12 10:24:13 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/solucaomobile/curso/resources/exception/RessourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.solucaomobile.curso.resources.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpServerErrorException;
+
+import com.solucaomobile.curso.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class RessourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServerErrorException request) {
+		
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		
+
+	}
+	
+}
