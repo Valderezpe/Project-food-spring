41dc350835129ca873f1835809cdb248fdede101
valderez <valderezpe@gmail.com>
2020-04-12 12:03:33 -0300

    Estado e Cidade

--- src/main/java/com/solucaomobile/curso/CursoApplication.java
+++ src/main/java/com/solucaomobile/curso/CursoApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.solucaomobile.curso.domain.Categoria;
+import com.solucaomobile.curso.domain.Cidade;
+import com.solucaomobile.curso.domain.Estado;
 import com.solucaomobile.curso.domain.Produto;
 import com.solucaomobile.curso.repositories.CategoriaRepository;
+import com.solucaomobile.curso.repositories.CidadeRepository;
+import com.solucaomobile.curso.repositories.EstadoRepository;
 import com.solucaomobile.curso.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursoApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
@@ -40,9 +48,22 @@ public class CursoApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 	    p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+	    
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+			
+		Estado est1 = new Estado(null, "Minas Gerais");
+	    Estado est2 = new Estado(null, "São Paulo");
+	    
+	    Cidade c1 = new Cidade(null, "Uberlandia", est1);
+	    Cidade c2 = new Cidade(null, "São Paulo", est2);
+	    Cidade c3 = new Cidade(null, "Campinas", est2);
+	    
+	    est1.getCidades().addAll(Arrays.asList(c1));
+	    est2.getCidades().addAll(Arrays.asList(c2, c3));
+	    
+	    estadoRepository.saveAll(Arrays.asList(est1, est2));
+	    cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	
 		
 		
