82597cb42030c7cbfa6e76bc5a9b9596634d63c0
valderez <valderezpe@gmail.com>
2020-05-01 12:20:35 -0300

    Personalizando o retorno da validação

--- src/main/java/com/solucaomobile/curso/resources/exception/StandardError.java
+++ src/main/java/com/solucaomobile/curso/resources/exception/StandardError.java
@@ -3,9 +3,9 @@ package com.solucaomobile.curso.resources.exception;
 import java.io.Serializable;
 
 public class StandardError implements Serializable {
-	
-	
 	private static final long serialVersionUID = 1L;
+	
+	
 	private Integer status;
 	private String msg;
 	private Long  timeStamp;
