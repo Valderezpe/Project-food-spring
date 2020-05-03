720c31167d736954549e3168519c68af4d0ba5f4
valderez <valderezpe@gmail.com>
2020-05-03 19:05:46 -0300

    Validação customizada: email não repedido na atualização cliente

--- src/main/java/com/solucaomobile/curso/dto/ClienteDTO.java
+++ src/main/java/com/solucaomobile/curso/dto/ClienteDTO.java
@@ -8,7 +8,10 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.solucaomobile.curso.domain.Cliente;
+import com.solucaomobile.curso.services.validation.ClienteUpdate;
 
+
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
